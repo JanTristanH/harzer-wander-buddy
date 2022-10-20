6a9bbff42081da0f4c1f21ac4b968289cb1587c5
Tristan <tristan.heisecke@implico.com>
2022-10-20 12:43:11 +0200

    spaghetti implementation toggeling

--- frontend-hwb/webapp/controller/Map.controller.js
+++ frontend-hwb/webapp/controller/Map.controller.js
@@ -9,27 +9,57 @@ sap.ui.define([
         const unstampedType = "Error";
 
         return Controller.extend("hwb.frontendhwb.controller.Map", {
-
+            itemCache: [],
             onInit: function () {
 
             },
             onToggleLables: function () {
+                //create item cache with unmodified items if not existent
+                this._getItemCache().length ? true : this._createInitialItemCache();
                 this.getView().byId("spots").getItems().map(e => e.setProperty("labelText", ""))
                 //TODO save labeltext to local set and make them displayable again
             },
+            _createInitialItemCache: function () {
+                //TODO this has to be reset on model Change urgh
+                this.itemCache = this.getView().byId("spots").getItems();
+            },
+            _getItemCache: function () {
+                return this.itemCache;
+            },
+            _clearItemCache: function () {
+                this.itemCache = [];
+            },
+            _resetItemsForSpots: function (spots) {
+                spots.removeAllItems();
+                this._getItemCache().forEach(item => {
+                    spots.addItem(item);
+                });
+            },
             onToggleStampedSpots: function () {
-                let spots = this.getView().byId("spots")
+                //create item cache with unmodified items if not existent
+                this._getItemCache().length ? true : this._createInitialItemCache();
+
+                let spots = this.getView().byId("spots");
+                this._resetItemsForSpots(spots);
                 spots.getItems()
                     .filter(e => e.getProperty("type") !== unstampedType)
                     .map(e => spots.removeItem(e))
                 //TODO reset items from global model
             },
             onShowGreens: function () {
+                //create item cache with unmodified items if not existent
+                this._getItemCache().length ? true : this._createInitialItemCache();
+
                 let spots = this.getView().byId("spots")
+                this._resetItemsForSpots(spots);
                 spots.getItems()
                     .filter(e => e.getProperty("type") === unstampedType)
                     .map(e => spots.removeItem(e))
                 //TODO reset items from global model
+            },
+            onShowAll: function () {
+                let spots = this.getView().byId("spots")
+                this._resetItemsForSpots(spots);
             }
         });
     });
