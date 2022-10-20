c94645a2c2ab9c1249d1b2f2743f2a36a06aa4ef
Tristan <tristan.heisecke@implico.com>
2022-10-20 10:34:04 +0200

    add removal of stamped spots and labels

--- frontend-hwb/webapp/controller/Map.controller.js
+++ frontend-hwb/webapp/controller/Map.controller.js
@@ -1,15 +1,28 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
+        "sap/ui/core/mvc/Controller"
+    ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
     function (Controller) {
         "use strict";
+        const unstampedType = "Error";
 
         return Controller.extend("hwb.frontendhwb.controller.Map", {
+
             onInit: function () {
 
+            },
+            onToggleLables: function () {
+                this.getView().byId("spots").getItems().map(e => e.setProperty("labelText", ""))
+                //TODO save labeltext to local set and make them displayable again
+            },
+            onToggleStampedSpots: function () {
+                let spots = this.getView().byId("spots")
+                spots.getItems()
+                    .filter(e => e.getProperty("type") == !unstampedType)
+                    .map(e => spots.removeItem(e))
+                //TODO reset items from global model
             }
         });
     });
