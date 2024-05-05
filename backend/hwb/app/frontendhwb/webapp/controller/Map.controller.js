f1e0c20e9194c1e52444300d89645681b2380d54
Tristan Heisecke <jan-tristan.heisecke@centric.eu>
2024-05-05 19:04:39 +0200

    Move Map Buttons to nice location (broken)

--- backend/hwb/app/frontendhwb/webapp/controller/Map.controller.js
+++ backend/hwb/app/frontendhwb/webapp/controller/Map.controller.js
@@ -1,15 +1,35 @@
 sap.ui.define([
-        "hwb/frontendhwb/controller/BaseController",
-    ],
+    "hwb/frontendhwb/controller/MapInner.controller",
+    "sap/ui/core/Fragment"
+],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Fragment) {
         "use strict";
 
         return Controller.extend("hwb.frontendhwb.controller.Map", {
             onAfterRendering: function () {
                 this.getView().byId("navButtonMapId").setType("Emphasized");
+            },
+            onPressOpenFiltersMenu: function (oEvent) {
+                var oButton = oEvent.getSource(),
+                    oView = this.getView();
+
+                // create popover
+                if (!this._pPopover) {
+                    this._pPopover = Fragment.load({
+                        id: oView.getId(),
+                        name: "hwb.frontendhwb.fragment.MapFilters",
+                        controller: this
+                    }).then(function (oPopover) {
+                        oView.addDependent(oPopover);
+                        return oPopover;
+                    });
+                }
+                this._pPopover.then(function (oPopover) {
+                    oPopover.openBy(oButton);
+                });
             }
         });
     });
