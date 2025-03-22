a8e48d3725f068dc570d7ba77be7cf080c4787ae
JanTristanH <tristan.heisecke@gmail.com>
2025-03-22 21:56:16 +0000

    create docker compose & simplify start script

--- backend/hwb/start.sh
+++ backend/hwb/start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-echo "Starting server..."
-npm start --production > /dev/null &
-echo "Server started, deploying..."
+echo "Deploying to database..."
 npm run deploy --production
-echo "Deployment complete, keeping container alive..."
-tail -f /dev/null
+echo "Deployment complete..."
+echo "Starting server..."
+npm start --production
